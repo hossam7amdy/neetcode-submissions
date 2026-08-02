9d5829bd53f7a544784385ef83eb2ca83dc2b58a
Hossam Hamdy <38383270+hossam7amdy@users.noreply.github.com>
2026-08-02 20:44:10 +0300

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.cpp
@@ -0,0 +1,19 @@
+class Solution {
+   public:
+    bool isPalindrome(string s) {
+        int l = 0, r = s.size() - 1;
+
+        while (l < r) {
+            while (l < r && !isalnum(s[l])) l++;
+            while (r > l && !isalnum(s[r])) r--;
+            if (tolower(s[l++]) != tolower(s[r--])) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
+
+/*
+"Was it a car or a cat I saw?"
+*/
